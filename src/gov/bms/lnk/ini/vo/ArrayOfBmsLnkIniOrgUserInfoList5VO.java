6412f6a0ba8484232735247c8cc8768c29a689da
turboguy <turboguy36@gmail.com>
2019-07-24 02:09:38 +0900

    v5

---
+++ src/gov/bms/lnk/ini/vo/ArrayOfBmsLnkIniOrgUserInfoList5VO.java
@@ -0,0 +1,18 @@
+/**
+ * Generated from schema type t=ArrayOfBmsLnkIniOrgUserInfoList5VO@java:gov.bms.lnk.ini.vo
+ */
+package gov.bms.lnk.ini.vo;
+
+public class ArrayOfBmsLnkIniOrgUserInfoList5VO implements java.io.Serializable {
+
+  private gov.bms.lnk.ini.vo.BmsLnkIniOrgUserInfoList5VO[] bmsLnkIniOrgUserInfoList5VO;
+
+  public gov.bms.lnk.ini.vo.BmsLnkIniOrgUserInfoList5VO[] getBmsLnkIniOrgUserInfoList5VO() {
+    return this.bmsLnkIniOrgUserInfoList5VO;
+  }
+
+  public void setBmsLnkIniOrgUserInfoList5VO(gov.bms.lnk.ini.vo.BmsLnkIniOrgUserInfoList5VO[] bmsLnkIniOrgUserInfoList5VO) {
+    this.bmsLnkIniOrgUserInfoList5VO = bmsLnkIniOrgUserInfoList5VO;
+  }
+
+}
---
+++ src/gov/bms/lnk/ini/vo/BmsLnkIniOrgUserInfoViewRes5VO.java
@@ -0,0 +1,28 @@
+/**
+ * Generated from schema type t=BmsLnkIniOrgUserInfoViewRes5VO@java:gov.bms.lnk.ini.vo
+ */
+package gov.bms.lnk.ini.vo;
+
+public class BmsLnkIniOrgUserInfoViewRes5VO implements java.io.Serializable {
+
+  private gov.bms.lnk.ini.vo.BmsLnkIniCommonResVO commonResVo;
+
+  public gov.bms.lnk.ini.vo.BmsLnkIniCommonResVO getCommonResVo() {
+    return this.commonResVo;
+  }
+
+  public void setCommonResVo(gov.bms.lnk.ini.vo.BmsLnkIniCommonResVO commonResVo) {
+    this.commonResVo = commonResVo;
+  }
+
+  private gov.bms.lnk.ini.vo.BmsLnkIniOrgUserInfo5VO orgUserInfo5Vo;
+
+  public gov.bms.lnk.ini.vo.BmsLnkIniOrgUserInfo5VO getOrgUserInfo5Vo() {
+    return this.orgUserInfo5Vo;
+  }
+
+  public void setOrgUserInfo5Vo(gov.bms.lnk.ini.vo.BmsLnkIniOrgUserInfo5VO orgUserInfo5Vo) {
+    this.orgUserInfo5Vo = orgUserInfo5Vo;
+  }
+
+}
